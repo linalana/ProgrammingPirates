badf5b7b496c1c1d795008de5fae60faef47f95f
MichaelHartle <Michael@lawn-128-61-77-116.lawn.gatech.edu>
2014-09-10 12:32:59 -0400

    Created files for player configuration, renamed welcome screen controller file

--- SpaceTrader/src/spacetrader/PlayerConfigurationController.java
+++ SpaceTrader/src/spacetrader/PlayerConfigurationController.java
@@ -8,26 +8,18 @@ package spacetrader;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author Michael
  */
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+public class PlayerConfigurationController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
