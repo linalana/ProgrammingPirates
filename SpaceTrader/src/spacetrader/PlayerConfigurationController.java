d655a6b80861d403478b0b4dfeac9c1c3f148dfc
linalana <lin.alanarose@gmail.com>
2014-09-11 00:11:43 -0400

    Modified DarkTheme to include smaller labels. Designed interface and set fx:id labels for needed controls.

--- SpaceTrader/src/spacetrader/PlayerConfigurationController.java
+++ SpaceTrader/src/spacetrader/PlayerConfigurationController.java
@@ -8,21 +8,46 @@ package spacetrader;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 /**
  * FXML Controller class
  *
- * @author Michael
+ * @author Alana Lin
  */
 public class PlayerConfigurationController implements Initializable {
-
+    @FXML 
+    private Button cancelButton;
+    private Button acceptButton;
+    private Slider fighterSlider;
+    private Slider traderSlider;
+    private Slider engineerSlider;
+    private Slider investorSlider;
+    private Label pointLabel;   
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    }  
+    @FXML
+    void UpdatePoints(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void createPlayer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnToStart(ActionEvent event) {
+
+    }
     
 }
