611517ebc742e035f0a9ecec2ce091d6a75725e0
vidmandan <vidmandan@yahoo.com>
2014-09-18 14:05:52 -0400

    Danny Gonzalez Work on Port, Bazaar and ShipYard

--- SpaceTrader/src/spacetrader/Port.java
+++ SpaceTrader/src/spacetrader/Port.java
@@ -11,5 +11,18 @@ package spacetrader;
  * @author Michael
  */
 public class Port {
+    private String name;
+    private ShipYard shipYard;
+    private Bazaar bazaar;
+    private int techLevel;
+    private int resources;
     
+    public Port(String newName, int newTechLevel, int newResources) {
+        name = newName;
+        techLevel = newTechLevel;
+        resources = newResources;
+        shipYard = new ShipYard();
+        bazaar = new Bazaar();
+        
+    }
 }
