3ffa5a30d964c4010f16f6d6d2034eaaf950d7da
linalana <lin.alanarose@gmail.com>
2014-09-18 14:09:22 -0400

    Started Constructor

--- SpaceTrader/src/spacetrader/Continent.java
+++ SpaceTrader/src/spacetrader/Continent.java
@@ -8,8 +8,22 @@ package spacetrader;
 
 /**
  *
- * @author Michael
+ * @author alanalin
  */
 public class Continent {
+    private String name;
+    private String politicalSystem;
+    private int techLevel;
+    private int resources;
+    private Port mainPort;
     
+    public Continent(String name, String politicalSystem) {
+        this.name = name;
+        this.politicalSystem = politicalSystem;
+        switch (politicalSystem) {
+            
+        }
+        
+        //mainPort = Port(name, techLevel, resources);
+    }
 }
