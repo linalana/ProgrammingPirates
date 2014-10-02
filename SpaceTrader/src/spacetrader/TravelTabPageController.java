3905dc1449f3721a0b85483285bc87957b69462e
MichaelHartle <Michael@lawn-128-61-74-54.lawn.gatech.edu>
2014-10-02 14:41:46 -0400

    Lots of UI/Controller Updates

--- SpaceTrader/src/spacetrader/TravelTabPageController.java
+++ SpaceTrader/src/spacetrader/TravelTabPageController.java
@@ -8,7 +8,12 @@ package spacetrader;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 /**
  * FXML Controller class
@@ -17,12 +22,30 @@ import javafx.fxml.Initializable;
  */
 public class TravelTabPageController implements Initializable {
 
+    @FXML
+    private ListView<String> portsList;
+    @FXML
+    private Label needed;
+    @FXML
+    private Label available;
+    private ObservableList<String> ports;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        available.setText("Fuel Available: " + Game.getPlayer().getShip().getFuel());
+        ports = portsList.getItems();
+        RangeChart range = new RangeChart();
+        Continent[] continents = range.getChart();
+        for (Continent c: continents) {
+            ports.add(c.getMainPort().toString());
+        }
     }    
     
+    @FXML
+    private void travelButtonPressed(ActionEvent event) {
+
+    }
 }
