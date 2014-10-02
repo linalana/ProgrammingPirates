805474d1882a0d3d4b13d94907c42bbb8709aa68
Murph <Murph@128.61.70.76>
2014-10-02 03:04:34 -0500

    Joe created a PoliceForce class with a strength and bribe level

---
+++ SpaceTrader/src/spacetrader/PoliceForce.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spacetrader;
+
+import java.util.Random;
+
+/**
+ * A police force that will be assigned to each continent
+ * @author Murph
+ */
+public class PoliceForce extends Encounterer{
+    int strength;
+    int bribeLevel;
+    
+    /**
+     * Constructor for PoliceForce
+     * @param system the political system of the continent
+     */
+    public PoliceForce(String system){
+        this.strength = calculateStrength(system);
+        this.bribeLevel = calculateBribe(system);
+    }
+    
+    /**
+     * 
+     * @param system the political system of the continent
+     * @return the strength
+     */
+    private int calculateStrength(String system){
+        int strength;
+        int strengthCount = 0;
+        String[] strengthArray = {"anarchy","feudal","pacifist","satori","capitalist","democracy","socialist",
+                                "confederacy","monarchy","communist","corporate","cybernetic","dictatorship",
+                                "technocracy","theocracy","fascist","military"};
+        for(int i=0; i<strengthArray.length; i++){
+            if(strengthArray[i].equals(system)){
+                strengthCount = i;
+            }
+        }
+        if(strengthCount>=0 && strengthCount<=1){
+            strength = 0;
+        }
+        else if(strengthCount>=2 && strengthCount<=3){
+            strength = 1;
+        }
+        else if(strengthCount>=4 && strengthCount<=6){
+            strength = 2;
+        }
+        else if(strengthCount>=7 && strengthCount<=8){
+            strength = 3;
+        }
+        else if(strengthCount>=9 && strengthCount<=14){
+            strength = 4;
+        }
+        else{
+            strength = 5;
+        }
+        return strength;
+    }
+    
+    /**
+     * 
+     * @param system the political system of the continent
+     * @return the bribe level
+     */
+    private int calculateBribe(String system){
+        int bribe;
+        int bribeCount = 0;
+        Random rand = new Random();
+        String[] bribeArray = {"anarchy","fascist","feudal","military","pacifist","satori","capitalist",
+                                "communist","cybernetic","democracy","dictatorship","monarchy","socialist",
+                                "confederacy","corporate","technocracy","theocracy"};
+        for(int i=0; i<bribeArray.length; i++){
+            if(bribeArray[i].equals(system)){
+                bribeCount = i;
+            }
+        }
+        if(bribeCount>=0 && bribeCount<=5){
+            bribe = 0;
+        }
+        else if(bribeCount>=6 && bribeCount<=10){
+            bribe = rand.nextInt((300 - 100) + 1) + 100;
+        }
+        else if(bribeCount>=11 && bribeCount<=12){
+            bribe = rand.nextInt((3000 - 1000) + 1) + 1000;
+        }
+        else{
+            bribe = rand.nextInt((30000 - 10000) + 1) + 10000;
+        }
+        return bribe;
+    }
+}
