7e4d139a0698e26224f580ffbe69844f0a9a31e8
James Mullenbach <james.mullenbach@gmail.com>
2014-10-30 16:27:17 -0400

    adding stuff to weapons tab

--- SpaceTrader/src/spacetrader/model/Weapon.java
+++ SpaceTrader/src/spacetrader/model/Weapon.java
@@ -47,9 +47,9 @@ public enum Weapon {
      * @param port
      * @return the price
      */
-    public int CalculatePrice(Port port) {
+    public int CalculatePrice(int techLevel) {
         Random rand = new Random();
-        int price = BasePrice + IPL * (port.getTechLevel() - MTLP) + BasePrice
+        int price = BasePrice + IPL * (techLevel - MTLP) + BasePrice
                      * ((rand.nextInt(2*Var) - Var / 2) / 100);
         if (price < 0) {
             return 0;
