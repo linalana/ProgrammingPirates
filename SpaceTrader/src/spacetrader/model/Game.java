7a6fbb941cd7fa78d805a3735acb19be6ee90671
Michael <Michael@michaels-mbp>
2014-11-03 20:24:37 -0500

    fixed some pmd flags for modelfacade

--- SpaceTrader/src/spacetrader/model/Game.java
+++ SpaceTrader/src/spacetrader/model/Game.java
@@ -37,6 +37,13 @@ public class Game implements Serializable {
     public static void setCurrentPort(final Port aCurrentPort) {
         currentPort = aCurrentPort;
     }
+    
+    /**
+     * @param aCurrentPort the currentPort to set
+     */
+    public static void setWorld(final World aWorld) {
+        world = aWorld;
+    }
 
     /**
      *
